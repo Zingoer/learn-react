4cc30b3630ab917d310a700a0da0b7b5375a2192
Xiaoxi Pang <pangxiaoxi@gmail.com>
2018-06-03 18:26:26 -0400

    Try render in every sec

--- learn-from-react-official-site/src/index.js
+++ learn-from-react-official-site/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+  }
+  
+setInterval(tick, 500);
 registerServiceWorker();
