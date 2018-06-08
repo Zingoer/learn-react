4fb362df4e08f8e897ea5a14f2b16c15b129fecf
Xiaoxi Pang <pangxiaoxi@gmail.com>
2018-06-07 20:17:22 -0400

    Add event handling function

--- learn-from-react-official-site/src/App.js
+++ learn-from-react-official-site/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Clock from "./Clock";
+import Toggle from "./Toggle";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         </p>
         <Clock name="Xiaoxi" increment={1}/>
         <Clock name="Yiyi" increment={2}/>
+        <Toggle />
       </div>
     );
   }
--- learn-from-react-official-site/src/Clock.js
+++ learn-from-react-official-site/src/Clock.js
@@ -8,6 +8,7 @@ class Clock extends React.Component {
       counter: 1
     };
     this.tick = this.tick.bind(this);
+    this.handleClickFakeLine = this.handleClickFakeLine.bind(this);
   }
 
   componentDidMount(){
@@ -21,11 +22,16 @@ class Clock extends React.Component {
     clearInterval(this.timerID);
   }
 
-  tick() {
+  tick = () => {
     this.setState((prevState, props) => ({
       date: new Date(),
       counter: prevState.counter + props.increment
     }));
+  }
+
+  handleClickFakeLine = (e) => {
+    e.preventDefault();
+    console.log('The link is clicked');
   }
 
   render() {
@@ -34,6 +40,7 @@ class Clock extends React.Component {
         <h1>Hello, this is {this.props.name}!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         <h3>The number is increment to {this.state.counter}</h3>
+        <a href="#" onClick={this.handleClickFakeLine}>This is a fake link</a>
       </div>
     );
   }
