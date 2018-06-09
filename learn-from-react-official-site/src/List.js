bfc14c9401f1d4ac3670e1bf6c6bf3b038bb7918
Xiaoxi Pang <pangxiaoxi@gmail.com>
2018-06-09 13:19:03 -0400

    Add key for the lists

--- learn-from-react-official-site/src/List.js
+++ learn-from-react-official-site/src/List.js
@@ -1,13 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ListItem from './ListItem';
 
-class List extends Component{
-    
-    render(){
-        return(
+class List extends Component {
+
+    render() {
+        return (
             <div>
-                Messages:  <br  />
-                {this.props.mails.length > 0 ? 
-                    <ol style={{display:'inline-block'}}>{this.props.mails.map((item) => <li>{item}</li>)}</ol> : 
+                Messages:  <br />
+                {this.props.mails.length > 0
+                    ?
+                    <ol style={{ display: 'inline-block' }}>
+                        {
+                            this.props.mails.map((item, index) => <ListItem key={index} value={item} />)
+                        }
+                    </ol>
+                    :
                     <div>Nothing entered yet!</div>}
             </div>
         )
