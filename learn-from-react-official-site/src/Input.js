f9d5228a8f86db9cc017aa5f6b8e96e12427227c
Yiyi <daiyiyi1991@gmail.com>
2018-06-11 21:45:39 -0400

    Change Input Component to uncontrolled version

--- learn-from-react-official-site/src/Input.js
+++ learn-from-react-official-site/src/Input.js
@@ -3,10 +3,6 @@ import React, { Component } from "react";
 class Input extends Component {
   constructor(props) {
     super(props);
-    this.word = "";
-    this.state = {
-      text: "xiaoxi"
-    };
   }
 
   handleChange = event => {
@@ -16,7 +12,7 @@ class Input extends Component {
   };
 
   handleClick = event => {
-    this.props.onClick(this.state.text.toUpperCase());
+    this.props.onClick(this.selectInput.value.toUpperCase());
     event.preventDefault();
   };
 
@@ -25,7 +21,9 @@ class Input extends Component {
       <form onSubmit={this.handleClick}>
         <label>
           Pick your best friend
-          <select value={this.state.text} onChange={this.handleChange}>
+          <select 
+            defaultValue='xiaoxi' 
+            ref={(selectInput) => this.selectInput = selectInput}>
             <option value="yiyi">Yiyi</option>
             <option value="xiaoxi">Xiaoxi</option>
           </select>
