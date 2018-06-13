b9b23729abcc86ce1a06ad5e11750b3b2077bdbd
Yiyi <daiyiyi1991@gmail.com>
2018-06-12 23:42:57 -0400

    Integrate the counter app with Redux

--- try-redux/src/App.js
+++ try-redux/src/App.js
@@ -1,51 +1,48 @@
-import React, { Component } from "react";
-import Title from "./Title";
-import Button from "./Button";
-import Count from "./Count";
-import './App.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Title from './Title';
+import Button from './Button';
+import Count from './Count';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
 
   handleAddClick = () => {
-    this.setState(prevState => {
-      return { count: prevState.count + 1 };
-    });
+    this.props.dispatch({type: 'INCREMENT'});
   };
 
   handleMinusClick = () => {
-    this.setState(prevState => {
-      return { count: prevState.count - 1 };
-    });
+    this.props.dispatch({type: 'DECREMENT'});
   };
 
   render() {
     return (
       <div id="Canvas">
-      <div className="App">
-        <Title className="Title" />
-        <div id="Counter-container">
-          <Button
-            className="Button"
-            buttonText="-"
-            handleClick={this.handleMinusClick}
-          />
-          <Count className="Count" countNumber={this.state.count} />
-          <Button
-            className="Button"
-            buttonText="+"
-            handleClick={this.handleAddClick}
-          />
+        <div className="App">
+          <Title className="Title" />
+          <div id="Counter-container">
+            <Button
+              className="Button"
+              buttonText="-"
+              handleClick={this.handleMinusClick}
+            />
+            <Count className="Count" countNumber={this.props.count}/>
+            <Button
+              className="Button"
+              buttonText="+"
+              handleClick={this.handleAddClick}
+            />
+          </div>
         </div>
-      </div>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- try-redux/src/index.js
+++ try-redux/src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+    count: 42
+}
+
+function reducer(state = initialState, action){
+    console.log(action);
+    switch(action.type) {
+        case 'INCREMENT':
+          return {
+            count: state.count + 1
+          };
+        case 'DECREMENT':
+          return {
+            count: state.count - 1
+          };
+        default:
+          return state;
+      }
+}
+
+const store = createStore(reducer);
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<ReduxApp />, document.getElementById('root'));
 registerServiceWorker();
